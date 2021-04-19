60c0b3fb8523bfbfb2b9d33af96dbd6042a04216
materight <matteo.est@gmail.com>
2021-04-19 22:02:30 +0200

    Init project structure

---
+++ classes/individual.py
@@ -0,0 +1,10 @@
+
+class Individual:
+    def __init__(self) -> None:
+        pass
+
+    def fitness(self, target) -> float:
+        pass
+
+    def mutate(self) -> None:
+        pass
---
+++ classes/model.py
@@ -0,0 +1,4 @@
+
+class Model():
+    def __init__(self, target_img_path) -> None:
+        pass
