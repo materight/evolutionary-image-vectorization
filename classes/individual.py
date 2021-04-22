4f8ef7b21876efc2534a90d4ccc07539ae8fe925
materight <matteo.est@gmail.com>
2021-04-22 17:08:43 +0200

    Update

--- classes/individual.py
+++ classes/individual.py
@@ -3,23 +3,52 @@ import cv2 as cv
 
 class Individual:
 
-    def __init__(self, img_shape, n_poly, n_vertex):
-        # Init random individual 
-        self.img_shape = img_shape
-        self.polys = np.random.randint((0, 0), img_shape[1::-1], (n_poly, n_vertex, 2))
-        self.colors = np.random.randint(0, 256, (n_poly, 4)) # R, G, B
-        self.alphas = np.full((n_poly), .2) # np.random.randint(0, .5, (n_poly))
+    def __init__(self, target, n_poly, n_vertex, genes=None): 
+        self.target = target
+        self.n_poly = n_poly
+        self.n_vertex = n_vertex
 
-    def _draw(self):
-        result = np.zeros(self.img_shape, np.uint8)
-        for pts, color, alpha in zip(self.polys, self.colors, self.alphas):
-            layer = np.zeros_like(result)
-            cv.fillPoly(layer, [pts], color.tolist())
-            result = cv.addWeighted(result, 1, layer, alpha, 0)
-        return result
+        # Init random individual
+        self._fitness = None
+        if genes is None:
+            self.genes = np.random.rand(n_poly * (n_vertex * 2 + 3 + 1))
+        else:
+            self.genes = genes
 
-    def fitness(self, target):
-        return np.sum((self.canvas - target)**2) 
 
-    def mutate(self):
-        pass
+    def crossover(parent1, parent2):
+        offspring = np.zeros_like(parent1.genes)
+        split_idx = np.random.randint(0, parent1.n_poly) * (parent1.n_vertex * 2 + 3 + 1)
+        for i, (g1, g2) in enumerate(zip(parent1.genes, parent2.genes)):
+            offspring[i] = g1 if i < split_idx else g2
+        return Individual(parent1.target, parent1.n_poly, parent1.n_vertex, offspring)
+        
+
+    def to_data(self):
+        polys = np.split(self.genes, self.n_poly)
+        polys = [np.split(poly, [-4, -1]) for poly in polys]
+        for i, poly in enumerate(polys):
+            polys[i][0] = (poly[0].reshape((self.n_vertex, 2)) * np.array([(self.target.shape[1] - 1), (self.target.shape[0] - 1)])).astype(int) # pts
+            polys[i][1] = (poly[1] * 255).astype(int) # color
+            polys[i][2] = (poly[2][0] * .05 + .3) # alpha between 0.05 and 0.3
+        return polys
+
+    def draw(self):
+        canvas = np.zeros_like(self.target)
+        for pts, color, alpha in self.to_data():
+            mask = cv.fillPoly(np.zeros_like(canvas), [pts], (1, 1, 1))
+            canvas = np.where(mask > 0, color * alpha + canvas * (1 - alpha), canvas).astype(np.uint8)
+        return canvas
+
+    def fitness(self):
+        if self._fitness is None:
+            canvas = self.draw()
+            self._fitness = np.sum((canvas - self.target)**2)
+        return self._fitness
+
+    def mutate(self, mutation_chance=0.1):
+        for i, gene in enumerate(self.genes):
+            if np.random.rand() < mutation_chance:
+                self.genes[i] = max(0, min(1, self.genes[i] + np.random.normal(scale=0.1)))
+        self._fitness = None
+
--- classes/population.py
+++ classes/population.py
@@ -4,11 +4,33 @@ import cv2 as cv
 from .individual import Individual
 
 class Population():
-    def __init__(self, target, pop_size=20, n_poly=100, n_vertex=3):
+    def __init__(self, target, pop_size=10, n_poly=50, n_vertex=3, selection_cutoff=.10):
+        self.generation = 0
+        self.selection_cutoff = selection_cutoff
         self.population = []
         for i in range(pop_size):
-            self.population.append(Individual(target.shape, n_poly, n_vertex))
-        pass
+            self.population.append(Individual(target, n_poly, n_vertex))
 
-    def evaluate():
-        
+    def next(self):
+        self.generation += 1
+        self.population.sort(key=lambda i: i.fitness())
+
+        # Selection
+        selected = self.population[0:int(len(self.population) * self.selection_cutoff)]
+
+        # Crossover
+        offspring = []
+        for i in range(len(self.population) - len(selected)):
+            offspring.append(Individual.crossover(
+                np.random.choice(selected),
+                np.random.choice(selected)
+            ))
+
+        # Mutation
+        self.population = selected + offspring
+        for i in self.population:
+            i.mutate()
+
+        # Return best individual
+        self.population.sort(key=lambda i: i.fitness())
+        return self.generation, self.population[0]
